8827390b09cd4f9304c902a3254438bf6a07c8e2
Marcush513 <102714188+Marcush513@users.noreply.github.com>
2022-05-07 15:20:25 -0400

    added better stylying. connected Bootstrap 5 to html and added different button. Added author of quote underneath quote

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const url = "https://api.quotable.io/random";
 let quote = document.getElementById("Welcome");
-
+const cite = document.querySelector("blockquote cite");
 // function to grab quote from API and creates quote in DOM
 const createQuote = () => {
   fetch(url)
     .then((data) => data.json())
     .then((item) => {
       quote.innerText = item.content;
+      cite.innerText = item.author;
     });
 };
 document.getElementById("button").addEventListener("click", createQuote);
+
+
